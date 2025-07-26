b1d24dc7e3ed3fc1589c7f7e9fdac4a72e36be52
PalmerGiblette <palmerdavidg@yahoo.com>
2025-07-25 22:58:12 -0600

    Fixed ubuntu AMI ID

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami             = "ami-0c55b159cbfafe1f0" # Ubuntu 22.04 in us-east-1
+  ami             = "ami-0c2b8ca1dad447f8a" # Ubuntu 22.04 in us-east-1
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.deployer.key_name
   security_groups = [aws_security_group.app_sg.name]
